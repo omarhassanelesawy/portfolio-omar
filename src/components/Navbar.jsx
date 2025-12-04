2da1841edb1f790f33346f0c04634b8912b2d26f
Omar Hassan Elesawy <49988341+omarhassanelesawy@users.noreply.github.com>
2025-12-04 08:00:04 +0200

    Navbar name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 0.5 }}
                     >
-                        Omar
+                        Omar Ele
                     </motion.a>
 
                     {/* Desktop Navigation */}
